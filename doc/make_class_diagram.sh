096c05390d71d63a72e8f075597b5e100d80fce4
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2026-05-06 12:27:18 +0800

    add script to generate class diagram

---
+++ doc/make_class_diagram.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+#sudo apt install plantuml        # the UML renderer ✓
+#sudo apt install doxygen         # ✓
+#sudo apt install graphviz        # ✓
+#sudo apt install cflow           # ✓
+#sudo apt install python3-pip     # needed for hpp2plantuml, but....
+
+### Ubuntu 24.04 is not happy about "pip install hpp2plantuml"
+
+#sudo apt install pipx     # needed for hpp2plantuml
+#pipx install hpp2plantuml
+#pipx ensurepath
+
+hpp2plantuml -i "../src/*.hpp" -o astroio.puml
+plantuml -tsvg astroio.puml
+inkscape --export-type=pdf --export-filename=astroio_class_diagram.pdf --export-area-drawing astroio.svg
